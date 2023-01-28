cc0a63b962504790f2d5f40c0f50b468ef2a1734
Li Feng Yin <lifeng-yin@proton.me>
2023-01-28 08:23:17 -0500

    Add auth pages

---
+++ src/pages/Signup/Signup.jsx
@@ -0,0 +1,34 @@
+import React, { useRef } from 'react';
+import { Link } from 'react-router-dom';
+
+export const Signup = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    
+  };
+
+  return (
+    <main>
+        <h1>Sign Up</h1>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="email">Email</label>
+            <input id="email" type="email" ref={emailRef} />
+
+            <label htmlFor="password">Password</label>
+            <input id="password" type="password" ref={passwordRef} />
+
+            <br />
+
+            <button type="submit">Sign up</button>
+        </form>
+
+        <p>Don&apos;t have an account? <Link to="/login">Log in</Link></p>
+    </main>
+  );
+};
+
+export default Signup;
