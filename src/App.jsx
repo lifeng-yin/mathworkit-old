22563cf0099e31763cc0bb33f71a810d4b248348
Li Feng Yin <lifeng-yin@proton.me>
2023-01-27 22:19:23 -0500

    Add folders to be used later on

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Landing from './pages/Landing/Landing';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Landing/>}></Route>
+        <Route path="*" element={<NotFound/>}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+    return (
+        <main>
+            <h1>404 Not Found</h1>
+        </main>
+    );
+};
+
+export default NotFound;
