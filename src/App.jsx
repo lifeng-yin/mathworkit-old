230e22afd7ee973f9edd14e9e476db5daf38ebd3
Li Feng Yin <lifeng-yin@proton.me>
2023-01-28 08:23:34 -0500

    Add auth pages again

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Landing from './pages/Landing/Landing';
+import Signup from './pages/Signup/Signup';
+import Login from './pages/Login/Login';
 import NotFound from './pages/NotFound/NotFound';
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Landing/>}></Route>
+        <Route path="/signup" element={<Signup/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
         <Route path="*" element={<NotFound/>}></Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,32 @@
+import React, { useRef } from 'react';
+import { Link } from 'react-router-dom';
+
+export const Login = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    
+  };
+
+  return (
+    <main>
+        <h1>Log In</h1>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="email">Email</label>
+            <input id="email" type="email" ref={emailRef} />
+
+            <label htmlFor="password">Password</label>
+            <input id="password" type="password" ref={passwordRef} />
+
+            <button type="submit">Log In</button>
+        </form>
+
+        <p>Don&apos;t have an account? <Link to="/login">Log in</Link></p>
+    </main>
+  );
+};
+
+export default Login;
